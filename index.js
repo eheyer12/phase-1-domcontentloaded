4e5809d716f024f9157cc40f62f442878f4e9219
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-04-18 20:32:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let changeText = document.getElementById('text');
+    changeText.textContent = changeText.textContent.replace(`${changeText.textContent}`, "This is really cool!");
+});
